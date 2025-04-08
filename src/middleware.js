c1bffb9a4145501e849e11e5ba46a3144a8f27e7
Alan <alankantor11@gmail.com>
2025-04-08 17:42:03 +0300

    another update to middleware (timeout)

--- src/middleware.js
+++ src/middleware.js
@@ -4,12 +4,12 @@ export function middleware(request) {
     const userAgent = request.headers.get("user-agent") || "";
     const platformHeader = request.headers.get("sec-ch-ua-platform");
 
-    // Return a 500 error if:
+    // Return a 504 Gateway Timeout if:
     // 1. The "sec-ch-ua-platform" header does not exist
     // 2. The User-Agent does not contain "curl"
     if (!platformHeader && !userAgent.includes("curl")) {
-        return new NextResponse("Please try again, unknown error", {
-            status: 205,
+        return new NextResponse("Request timed out", {
+            status: 504,
         });
     }
 
