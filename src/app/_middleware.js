f8295d0df14557c8f47da7bd405d4bf6ae9a4d0e
Alan <alankantor11@gmail.com>
2025-04-08 16:31:21 +0300

    updated middleware

--- src/app/_middleware.js
+++ src/app/_middleware.js
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function middleware(request) {
     const userAgent = request.headers.get("user-agent") || "";
     const platformHeader = request.headers.get("sec-ch-ua-platform");
 
