621ba6ddd7e23752c690fc060b66ebaea9b12c5c
Alan <alankantor11@gmail.com>
2025-04-08 17:59:59 +0300

    change middleware to 403

--- src/middleware.js
+++ src/middleware.js
@@ -4,12 +4,12 @@ export function middleware(request) {
     const userAgent = request.headers.get("user-agent") || "";
     const platformHeader = request.headers.get("sec-ch-ua-platform");
 
-    // Return a 504 Gateway Timeout if:
+    // Return a 403 Forbidden if:
     // 1. The "sec-ch-ua-platform" header does not exist
     // 2. The User-Agent does not contain "curl"
     if (!platformHeader && !userAgent.includes("curl")) {
-        return new NextResponse("Request timed out", {
-            status: 504,
+        return new NextResponse("Forbidden", {
+            status: 403,
         });
     }
 
